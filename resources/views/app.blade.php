da5077f4eeedc5acd53a9d2e4f4550867d545a69
dipeshkhatiwada <depeshkhatiwada@gmail.com>
2021-07-26 20:42:20 +0545

    laravcel inertia

---
+++ resources/views/app.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0"/>
+    <link href="" rel="stylesheet"/>
+    <script src="" defer></script>
+</head>
+<body>
+
+<div class="container">
+    <div class="row">
+        <div class="col-md-12">
+            <h2 class="app-title">Laravel Inertia CRUD Test</h2>
+        </div>
+    </div>
+</div>
+<hr>
+<div class="container">
+    <div class="row">
+        @inertia
+    </div>
+</div>
+
+</body>
+</html>
